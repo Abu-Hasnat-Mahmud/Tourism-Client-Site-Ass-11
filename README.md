a722e6ec445f828c9c88a7920f4e41ecae4bdd37
himu <si11@gtrbd.com>
2021-12-10 19:30:01 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Tour
+
+[Tour](https://tourism-a9bfc.web.app/)
+
+We provoide quality and minimum cost tour package
+
+- 100+ spots
+- Free transport
+- Purchase package easily
+- Good client feedback
+- 20+ branches
---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCMO1gewkxgyqA1OxJDRX_NiIykd8NSx4A",
+  authDomain: "tourism-a9bfc.firebaseapp.com",
+  projectId: "tourism-a9bfc",
+  storageBucket: "tourism-a9bfc.appspot.com",
+  messagingSenderId: "362064575241",
+  appId: "1:362064575241:web:c3d239bd05578792dab1d8",
+};
+
+export default firebaseConfig;
